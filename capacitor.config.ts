7dff98a3956905ccbe80bc823ba6849c83e67a67
WaxmenWork <AleksandrovichPA@mer.ci.nsu.ru>
2023-03-29 22:50:11 +0700

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'foody-rest',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
